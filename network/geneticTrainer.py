68ca197ada9187247d930c5bdde282e37771e210
kn2282 <57344058+kn2282@users.noreply.github.com>
2021-06-10 13:41:57 +0200

    geneticTrainer.py added

---
+++ network/geneticTrainer.py
@@ -0,0 +1,13 @@
+from network.nn import NeuralNetwork
+
+
+class geneticTrainer:
+    def __init__(self):
+        best = NeuralNetwork.rand()
+        pass
+
+
+
+    def make_round(self):
+        #ToDo:
+        pass
